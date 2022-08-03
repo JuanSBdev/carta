7388e1a8b6cef21c0c5d00bc3a9a0221b66660f9
Juan SB <juansb.dev@gmail.com>
2022-08-02 21:47:12 -0300

    showExtras

--- index.js
+++ index.js
@@ -4,27 +4,25 @@ let no = document.getElementsByClassName('none');
 
 function addProductTexas(){
     let listaPadre = document.getElementById("orden");
-    let noTex = document.getElementsByClassName('noneTex');
-    noTex[0].classList.add('show');
-
-    console.log(listaPadre);
-
-listaPadre.innerHTML +=  pedidos[0].innerHTML;
+    no[0, 1].classList.add('show');
+    listaPadre.innerHTML +=  pedidos[0].innerHTML;
 
 };
 function addProductCali(){
     let listaPadre = document.getElementById("orden");
     listaPadre.innerHTML +=   pedidos[1].innerHTML;
+    no[2, 3].classList.add('show');
 
 };
 function addProductNY(){
     let listaPadre = document.getElementById("orden");
-    console.log(listaPadre);
+    no[4, 5].classList.add('show');
+
     listaPadre.innerHTML +=   pedidos[2].innerHTML;
 };
 function addProductH(){
     let listaPadre = document.getElementById("orden");
-    console.log(listaPadre);
+    no[6, 7].classList.add('show');
     listaPadre.innerHTML +=   pedidos[3].innerHTML;
 }
 /* aca hay que hacer una funcion [i] para no tener que poner una funcion x*/
