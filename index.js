5be226232c6edf937b6cda891598581080e126a2
Juan SB <juansb.dev@gmail.com>
2022-11-15 00:14:34 -0300

    kart3

--- index.js
+++ index.js
@@ -7,18 +7,41 @@ let extras = document.getElementById('extra')
 function addProductTexas(){
     
     Swal.fire({
-        title:'Agregaste una Texas Burguer a tu pedido',
+        title:'Agregaste una texas a tu pedido :)',
         color: 'orange',
         showCancelButton: true,
         showConfirmButton: true,
-        confirmButtonText: 'ok',
-    confirmButton:'btn btn-light',
-} );
-let nuev = document.createElement('div');
+        confirmButtonText: 'Ok',
+        cancelButtonText: 'Quitar',
+    } ).then((result) => {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {
+          Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'Agregado',
+            showConfirmButton: false,
+            timer: 1500
+
+          });
+          let nuev = document.createElement('div');
 let listaPadre = document.getElementById("pedido");
 nuev.innerHTML = pedidos[0].innerHTML;
 nuev.classList.add('orden');
 listaPadre.appendChild(nuev);
+        }
+        else if (result.isDenied) {
+        }
+        else{        Swal.fire({
+            position: 'center',
+            icon: 'info',
+            title: 'Nope!',
+            showConfirmButton: false,
+            timer: 1500
+          })
+    }
+      });
+
 //la funcion antes:
 //    let listaPadre = document.getElementById("pedido");
 //listaPadre.innerHTML +=  pedidos[0].innerHTML;
@@ -26,68 +49,157 @@ listaPadre.appendChild(nuev);
 };
 function addProductCali(){
     Swal.fire({
-        title:'Agregaste una California Burguer a tu pedido',
+        title:'Agregaste una cali a tu pedido :)',
         color: 'orange',
         showCancelButton: true,
         showConfirmButton: true,
-        confirmButtonText: 'ok',
-        confirmButton:'btn btn-light',
-    } );
-    let nuev = document.createElement('div');
+        confirmButtonText: 'Ok',
+        cancelButtonText: 'Quitar',
+    } ).then((result) => {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {
+          Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'Agregado',
+            showConfirmButton: false,
+            timer: 1500
+
+          });
+          let nuev = document.createElement('div');
 let listaPadre = document.getElementById("pedido");
 nuev.innerHTML = pedidos[1].innerHTML;
 nuev.classList.add('orden');
 listaPadre.appendChild(nuev);
+        }
+        else if (result.isDenied) {
+        }
+        else{        Swal.fire({
+            position: 'center',
+            icon: 'info',
+            title: 'Nope!',
+            showConfirmButton: false,
+            timer: 1500
+          })
+    }
+      });
+
+
    
 };
 function addProductFried(){
     Swal.fire({
-        title:'Agregaste Super Pancho a tu pedido',
+        title:'Agregaste Fried Chicken a tu pedido',
         color: 'orange',
         showCancelButton: true,
         showConfirmButton: true,
-        confirmButtonText: 'ok',
-        confirmButton:'btn btn-light',
-    } );
+        confirmButtonText: 'Ok',
+        cancelButtonText: 'Quitar',
+    } ).then((result) => {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {
+          Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'Agregado',
+            showConfirmButton: false,
+            timer: 1500
 
-let nuev = document.createElement('div');
+          });
+          let nuev = document.createElement('div');
 let listaPadre = document.getElementById("pedido");
 nuev.innerHTML = pedidos[4].innerHTML;
 nuev.classList.add('orden');
 listaPadre.appendChild(nuev);
+        }
+        else if (result.isDenied) {
+        }
+        else{        Swal.fire({
+            position: 'center',
+            icon: 'info',
+            title: 'Nope!',
+            showConfirmButton: false,
+            timer: 1500
+          })
+    }
+      });
+
+
 };
 
 function addProductPancho(){
     Swal.fire({
-        title:'Agregaste Super Pancho a tu pedido',
+        title:'Agregaste un pancho a tu pedido',
         color: 'orange',
         showCancelButton: true,
         showConfirmButton: true,
-        confirmButtonText: 'ok',
-        confirmButton:'btn btn-light',
-    } );
+        confirmButtonText: 'Ok',
+        cancelButtonText: 'Quitar',
+    } ).then((result) => {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {
+          Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'Agregado',
+            showConfirmButton: false,
+            timer: 1500
 
-let nuev = document.createElement('div');
+          });
+          let nuev = document.createElement('div');
 let listaPadre = document.getElementById("pedido");
 nuev.innerHTML = pedidos[5].innerHTML;
 nuev.classList.add('orden');
 listaPadre.appendChild(nuev);
+        }
+        else if (result.isDenied) {
+        }
+        else{        Swal.fire({
+            position: 'center',
+            icon: 'info',
+            title: 'Nope!',
+            showConfirmButton: false,
+            timer: 1500
+          })
+    }
+      });
 };
 function addProductPapas(){
     Swal.fire({
-        title:'Agregaste Papas Tabak a tu pedido',
+        title:'Agregaste papas a tu pedido',
         color: 'orange',
         showCancelButton: true,
         showConfirmButton: true,
-        confirmButtonText: 'ok',
-        confirmButton:'btn btn-light',
-    } );
+        confirmButtonText: 'Ok',
+        cancelButtonText: 'Quitar',
+    } ).then((result) => {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {
+          Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'Agregado',
+            showConfirmButton: false,
+            timer: 1500
 
-    let nuev = document.createElement('div');
+          });
+          let nuev = document.createElement('div');
 let listaPadre = document.getElementById("pedido");
 nuev.innerHTML = pedidos[6].innerHTML;
 nuev.classList.add('orden');
 listaPadre.appendChild(nuev);
+        }
+        else if (result.isDenied) {
+        }
+        else{        Swal.fire({
+            position: 'center',
+            icon: 'info',
+            title: 'Nope!',
+            showConfirmButton: false,
+            timer: 1500
+          })
+    }
+      });
 
 };
 
